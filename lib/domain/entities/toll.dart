3d7a7d372f2b95ce6055a31c3b371ec65a0f133f
dradian-cyber <85037243+dradian-cyber@users.noreply.github.com>
2025-03-14 02:55:47 -0500

    Update toll.dart

--- lib/domain/entities/toll.dart
+++ lib/domain/entities/toll.dart
@@ -1,3 +1,5 @@
+/// This file defines the Toll class, which represents a toll entity with
+/// properties for id, location, amount, and timestamp.
 class Toll {
   final String id;
   final String location;

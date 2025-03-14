48317fd36860021e598c1a0c97714f1b4d384faf
dradian-cyber <85037243+dradian-cyber@users.noreply.github.com>
2025-03-14 03:44:02 -0500

    Update toll_page.dart

--- lib/presentation/pages/toll_page.dart
+++ lib/presentation/pages/toll_page.dart
@@ -1,3 +1,6 @@
+//The View complies with the ViewModel and updates the UI when the state changes
+//This is a Toll Page example
+//
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../viewmodels/toll_viewmodel.dart';
